a4a1e19039e596e948a006f167abc8499cef0fa7
Logan Connor <LoganConnor44@gmail.com>
2018-04-02 22:05:07 +0000

    added new challenge - Dont Give Me Five

---
+++ CodeKatas.Tests/DontGiveMeFiveTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.Tests
+{
+    [TestClass]
+    class DontGiveMeFiveTests
+    {
+        [TestMethod]
+        public void TestNoFives()
+        {
+            Assert.AreEqual(8, DontGiveMeFive.NoFives(1, 9));
+            Assert.AreEqual(12, DontGiveMeFive.NoFives(4, 17));
+        }
+    }
+}
---
+++ CodeKatas/DontGiveMeFive/DontGiveMeFive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKatas
+{
+    public class DontGiveMeFive
+    {
+        public static int NoFives(int start, int end)
+        {
+            return 0;
+        }
+    }
+}
