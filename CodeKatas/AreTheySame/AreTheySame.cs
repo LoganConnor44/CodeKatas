8f6748034efe6f2849d342706cebec8ef9cf50bf
Logan Connor <LoganConnor44@gmail.com>
2018-04-02 20:40:17 +0000

    added comments

--- CodeKatas/AreTheySame/AreTheySame.cs
+++ CodeKatas/AreTheySame/AreTheySame.cs
@@ -2,8 +2,17 @@
 
 namespace CodeKatas
 {
+    /// <summary>
+    /// Checks whether the two arrays have the "same" elements, with the same multiplicities.
+    /// </summary>
     public class AreTheySame
     {
+        /// <summary>
+        /// Compares that the elements in b are the elements in a squared, regardless of the order.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static bool Comp(int[] a, int[] b)
         {
             foreach(int item in a)
